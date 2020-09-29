bc7aa7ce4af1e509969f304feff9643fa5a6b6ab
kumarabhishek404 <abhishek19@navgurukul.org>
2020-09-29 13:22:57 +0530

    Updates

---
+++ precache-manifest.c5d8eabeda9f669cba5ffc4305a8e7e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0fbdcece61bd3a5bf9e7c8531df7df",
+    "url": "/animadrive-product/index.html"
+  },
+  {
+    "revision": "bd650b3a5ddee1e48e76",
+    "url": "/animadrive-product/static/css/main.87d91fdd.chunk.css"
+  },
+  {
+    "revision": "b3aa528d37ad32aa65c8",
+    "url": "/animadrive-product/static/js/2.6b405073.chunk.js"
+  },
+  {
+    "revision": "ce15855f217da1bc69425dbb3111adff",
+    "url": "/animadrive-product/static/js/2.6b405073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd650b3a5ddee1e48e76",
+    "url": "/animadrive-product/static/js/main.0e692809.chunk.js"
+  },
+  {
+    "revision": "07f2cd9bea4d17804cf7",
+    "url": "/animadrive-product/static/js/runtime-main.eaad0d75.js"
+  }
+]);
