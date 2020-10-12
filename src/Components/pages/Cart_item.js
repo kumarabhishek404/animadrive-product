d71bbc324008b2f75cac252c08a020fb792c004d
kumarabhishek404 <abhishek19@navgurukul.org>
2020-10-12 09:52:53 +0530

    "Himanshu conflict resolved"

--- src/Components/pages/Cart_item.js
+++ src/Components/pages/Cart_item.js
@@ -1,57 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import Button from '../Button'
 import './Cart.css'
 
-function Cart_item({ productName, product_src, price }) {
+function Cart_item({ productName, product_src, price, ProductQuantity, onProductTotal, onProductQuantity }) {
 
-    const [quantity, setQuantity] = useState(1)
+    const [quantity, setQuantity] = useState(ProductQuantity)
+    const [productPrice, setProductPrice] = useState(price)
+    // const [singlePrice, setSinglePrice] = useState(price)
 
-    const quantityIncrement = () => setQuantity(prevCount => prevCount + 1)
-    const quantityDecrement = () => {
-        (quantity > 0)
-            ? setQuantity(prevCount => prevCount - 1)
-            : setQuantity(0)
+    const handleQuantityIncrement = () => {
+        setQuantity(prevCount => prevCount + 1)
+        setProductPrice((price * quantity) + price)
+        // setSinglePrice(price * quantity)
+        // onProductTotal(singlePrice)
     }
 
+    const handleQuantityDecrement = () => {
+        if (quantity > 1) {
+            setQuantity(prevCount => prevCount - 1)
+            setProductPrice(productPrice - price)
+
+        }
+    }
+
+    useEffect(() => {
+        setProductPrice(price * ProductQuantity)
+        onProductTotal(price * ProductQuantity)
+        onProductQuantity(quantity)
+    }, [])
 
     return (
         <div className='product_item_list'>
             <div className='product_item_container'>
                 <div className='cart_product_image'>
                     <figure className='product_image_wrap'>
-                        <img src={product_src} className='cart_image_single' />
+                        <img src={product_src} />
                     </figure>
-<<<<<<< HEAD
                     <h2>{`₹${price}`}</h2>
                 </div>
                 <div className='cart_product_info'>
                     <h2>{productName}</h2>
-=======
-                    <h2>{price}</h2>
-                </div>
-                <div className='cart_product_info'>
-                    <h2>{productName}</h2>
-                    <h2 className='price_head'>{price}</h2>
+                    <h2 className='price_head'>{`₹${productPrice}`}</h2>
                 </div>
                 <div className='product_action'>
->>>>>>> f803b1623fc34a58f416a1b7300b15a986511c6f
                     <div className='product_quantity'>
-                        <RemoveIcon onClick={quantityDecrement} /><h3>{quantity}</h3><AddIcon onClick={quantityIncrement} />
-                    </div> <div className="products">
-                <div className='products_container'>
-                    <h1>OUR PRODUCTS</h1>
-                    <div className='all_products'>
-                            {/* {itemList} */}
+                        <RemoveIcon onClick={handleQuantityDecrement} /><h3>{quantity}</h3><AddIcon onClick={handleQuantityIncrement} />
                     </div>
-                </div>
-<<<<<<< HEAD
-                <div className='product_action'>
-                    <h2 className='price_head'>{`${productPrice}`}</h2>
-=======
-            </div>
->>>>>>> f803b1623fc34a58f416a1b7300b15a986511c6f
                     <div>
                         <Button>Remove</Button>
                     </div>
